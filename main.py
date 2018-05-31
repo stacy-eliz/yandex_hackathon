92f4f40b60a8e2fe63359a3e489173cd0daf65bf
Dikower <dmitriy1d01@gmail.com>
2018-05-31 22:06:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ from __future__ import unicode_literals
 # Импортируем модули для работы с логами.
 import logging
 
+# Импортируем модуль для работы с json
+import json
+
 # Импортируем модуль для работы с API Алисы
 from alice_sdk import AliceRequest, AliceResponse
 
@@ -19,23 +22,36 @@ app = Flask(__name__)
 logging.basicConfig(level=logging.DEBUG)
 
 # Хранилище данных о сессиях.
+
 session_storage = {}
 
-
+# Операвтивная памятьна heroku не стабильна, поэтому придется сохраняться на диск
+with open("sessions.json", "w", encoding="utf8") as file:
+    json.dump(session_storage, fp=file)
+    
 # Задаем параметры приложения Flask.
-@app.route("/", methods=['POST'])
+@app.route("/", methods=["POST"])
 def main():
     # Функция получает тело запроса и возвращает ответ.
+    with open("sessions.json", encoding="utf8") as file:
+        session_storage = json.loads(file.read())
+
     alice_request = AliceRequest(request.json)
-    logging.info('Request: {}'.format(alice_request))
+    logging.info("Request: {}".format(alice_request))
 
     alice_response = AliceResponse(alice_request)
 
     user_id = alice_request.user_id
+
     alice_response, session_storage[user_id] = handle_dialog(
-            alice_request, alice_response, session_storage.get(user_id)
-        )
-    logging.info('Response: {}'.format(alice_response))
+        alice_request, alice_response, session_storage.get(user_id)
+    )
+    
+    # Потому что оперативка плохо работает
+    with open("sessions.json", "w", encoding="utf8") as file:
+        json.dump(session_storage, fp=file)
+
+    logging.info("Response: {}".format(alice_response))
 
     return alice_response.dumps()
 
